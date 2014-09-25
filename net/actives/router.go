8d86160822f49d4563bc6d5d7efdd9e2f5f929b4
Andre Luis Pereira dos Santos <andre@bsrpar.com>
2014-09-25 13:06:53 -0300

    Improvementsin development of tests and path to router device simulator

--- net/actives/router.go
+++ net/actives/router.go
@@ -3,6 +3,18 @@ package router
 //Authorize the route between a from and a to
 //This make the function of a router device
 //Uses a from string with a IP address and a to string with a IP address
-func Routing(from string, to string) {
+//Return a boolean with authorization ornot to routing (if the route
+//exists at routing table
+func Routing(from string, to string) bool {
+
+	routingTable := make(map[string]bool)
+	routingTable["192.168.1.65"] = true
+
+	if from != "" && routingTable[to] == true {
+
+		return true
+	} else {
+		return false
+	}
 
 }
--- net/actives/router_test.go
+++ net/actives/router_test.go
@@ -6,7 +6,7 @@ func TestRouting(t *testing.T) {
 
 	var pass bool
 
-	pass = Routing(1, 2)
+	pass = Routing("192.168.1.5", "192.168.1.65")
 
 	if pass != true {
 		t.Error("Expected true, got ", pass)
