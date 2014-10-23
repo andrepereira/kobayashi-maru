94a64e8198306588c045047f51bf585fd8bbf052
Andre Luis Pereira dos Santos <andre@bsrpar.com>
2014-10-23 13:10:51 -0200

    Comments included at web interface module

--- interface/web.go
+++ interface/web.go
@@ -10,13 +10,15 @@ import (
 )
 
 func main() {
+	//Web server to interface of all system
+	//Using Martini web framework
 	m := martini.Classic()
 	m.Use(render.Renderer())
 
 	m.Get("/wishes", func(r render.Render) {
 		r.HTML(200, "standard", nil)
 	})
-
+	//To portability, included at cloud enviorements is taked env. variables to IP/Host and Port to server run
 	bind := fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
 	fmt.Printf("listening on %s...", bind)
 	err := http.ListenAndServe(bind, m)
